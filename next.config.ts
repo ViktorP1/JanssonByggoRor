60cb8b89f3351c4fc53e1ff24731060b89127a5e
Viktor Pavlov <viktorpavlov001122@gmail.com>
2025-12-01 15:26:58 +0100

    Commit

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production"
+const repoName = "JanssonByggoRor"
+
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
